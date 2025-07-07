0c597c458dd236176ba090a399f90635dba30050
George Leonard <georgelza@gmail.com>
2025-07-07 20:46:49 +0200

    expanded on examples in creFlinkFlows

--- devlab0/creFlinkFlows/4.1.creSnmp_poll_data_FlussInserts.sql
+++ devlab0/creFlinkFlows/4.1.creSnmp_poll_data_FlussInserts.sql
@@ -46,3 +46,48 @@ SELECT
     ,CAST(ts AS STRING)                             AS ts -- Cast the TIMESTAMP(3) to STRING for the target table
     ,DATE_FORMAT(ts, 'yyyyMM')                      AS partition_month -- Directly format the TIMESTAMP column
 FROM hive.snmp.snmp_poll_data3;
+
+
+SET 'pipeline.name' = 'Push SNMP snmp_poll_data4 => fluss.snmp_poll_data';
+
+INSERT INTO fluss.snmp.snmp_poll_data
+    (device_id, metric_oid, metric_value, data_type, instance_identifier, ts, partition_month)
+SELECT
+     device_id                                      AS device_id
+    ,metric_oid                                     AS metric_oid      
+    ,metric_value                                   AS metric_value
+    ,data_type                                      AS data_type
+    ,instance_identifier                            AS instance_identifier
+    ,CAST(ts AS STRING)                             AS ts -- Cast the TIMESTAMP(3) to STRING for the target table
+    ,DATE_FORMAT(ts, 'yyyyMM')                      AS partition_month -- Directly format the TIMESTAMP column
+FROM hive.snmp.snmp_poll_data4;
+
+
+SET 'pipeline.name' = 'Push SNMP snmp_poll_data5 => fluss.snmp_poll_data';
+
+INSERT INTO fluss.snmp.snmp_poll_data
+    (device_id, metric_oid, metric_value, data_type, instance_identifier, ts, partition_month)
+SELECT
+     device_id                                      AS device_id
+    ,metric_oid                                     AS metric_oid      
+    ,metric_value                                   AS metric_value
+    ,data_type                                      AS data_type
+    ,instance_identifier                            AS instance_identifier
+    ,CAST(ts AS STRING)                             AS ts -- Cast the TIMESTAMP(3) to STRING for the target table
+    ,DATE_FORMAT(ts, 'yyyyMM')                      AS partition_month -- Directly format the TIMESTAMP column
+FROM hive.snmp.snmp_poll_data5;
+
+
+SET 'pipeline.name' = 'Push SNMP snmp_poll_data6 => fluss.snmp_poll_data';
+
+INSERT INTO fluss.snmp.snmp_poll_data
+    (device_id, metric_oid, metric_value, data_type, instance_identifier, ts, partition_month)
+SELECT
+     device_id                                      AS device_id
+    ,metric_oid                                     AS metric_oid      
+    ,metric_value                                   AS metric_value
+    ,data_type                                      AS data_type
+    ,instance_identifier                            AS instance_identifier
+    ,CAST(ts AS STRING)                             AS ts -- Cast the TIMESTAMP(3) to STRING for the target table
+    ,DATE_FORMAT(ts, 'yyyyMM')                      AS partition_month -- Directly format the TIMESTAMP column
+FROM hive.snmp.snmp_poll_data6;
